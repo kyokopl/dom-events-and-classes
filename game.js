ff49b8930f2b2e8152b78b8c3b1fdd2a3a8900b1
Kyoko Pohe <Kyokz@Kyokos-MacBook-Pro.local>
2018-06-29 12:56:47 +1200

    added counters

--- game.js
+++ game.js
@@ -43,6 +43,16 @@ function updateCounts () {
   }
   
   // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
+  var totalDots = document.getElementsByClassName('board')[0].children
+    for (var i = 0; i < totalDots.length; i++) {
+      if (totalDots[i].classList.contains('blue')) {
+      totals.blue++ 
+    } else if (totalDots[i].classList.contains('green')) {
+      totals.green++
+    } else if (totalDots[i].classList.contains('invisible')) {
+      totals.invisble++
+    }
+  }
 
   // Once you've done the counting, this function will update the display
   displayTotals(totals)
